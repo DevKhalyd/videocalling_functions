525b368c8273e4f1b1d3f20413d7ac4d9de1f41d
Rolando Garcia <jrdosmil0@gmail.com>
2021-10-07 09:13:18 -0500

    Minor improvements

---
+++ README.md
@@ -0,0 +1,9 @@
+# Functions for Videocalling App
+
+Basically reacts to the changes in the database
+
+# Commands
+
+`firebase emulators:start` Start to emulate
+
+**See all the commands avaible:** https://firebase.google.com/docs/emulator-suite/install_and_configure
--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,43 @@
 // Example of how to run and deploy functions
 // https://firebase.google.com/docs/functions/get-started#set-up-node.js-and-the-firebase-cli
+
+// Java Error:
+//https://stackoverflow.com/questions/56819840/firebase-cloud-functions-emulator-throws-exited-with-code-1-error
+
+// Examples:
+// https://firebase.google.com/docs/firestore/extend-with-functions#specific-documents
 const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const usersCollection = 'users'
+
+// Listens for new messages added to users collection
+// Then
+exports.encryptUserPassword = functions.firestore.document(`/${usersCollection}/{documentId}`)
+    .onCreate((snap, _) => {
+        const data = snap.data();
+        let password = data.password;
+        password = `${password}-encrypt this one`
+        functions.logger.log('Data in the map', data);
+        // You must return a Promise when performing asynchronous tasks inside of Functions.
+        // TODO: Add to the collections usernames this username as busy
+        return snap.ref.update({ password });
+    });
 
 /*
-The Firebase CLI automatically installs the Firebase and Firebase SDK 
-for Cloud Functions Node modules when you initialize your project. 
-To add 3rd party libraries to your project, you can modify 
-package.json and run npm install.For more information, 
+The Firebase CLI automatically installs the Firebase and Firebase SDK
+for Cloud Functions Node modules when you initialize your project.
+To add 3rd party libraries to your project, you can modify
+package.json and run npm install.For more information,
 see Handle Dependencies. (https://firebase.google.com/docs/functions/handle-dependencies)
 */
 
+
+/*
+Use bcrypt
+https://www.npmjs.com/package/bcrypt
+More: https://firebase.google.com/docs/firestore/extend-with-functions
+*/
